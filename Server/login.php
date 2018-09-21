52fadf7ece9b46671791bc42c7a5f2fef2420acd
Eduardo <41192980+blueudp@users.noreply.github.com>
2018-09-21 10:08:09 +0200

    Update login.php

--- Server/login.php
+++ Server/login.php
@@ -7,22 +7,19 @@ include('db.inc.php');
 $sess = @($_SESSION['username']);
 
 if(isset($sess)) {
-
-   header("Location: index.php");
-
+    header("Location: index.php");
 } else {
 
-echo '<html><head><title>Login</title></head><body bgcolor="black"><center><h1 style="color: red;">RootPhisher</h1><h1 style="color: lime;">Login</h1><form action="#" method="POST"><input style="color: lime; background: black;" type="text" placeholder="Username" name="user"><br><br><input style="color: lime; background: black;" type="password" placeholder="Password" name="pass"><br><br><input style="color: lime; background: black;" type="submit" value="Login" name="login"></center></body></html>';
+    echo '<html><head><title>Login</title></head><body bgcolor="black"><center><h1 style="color: red;">RootPhisher</h1><h1 style="color: lime;">Login</h1><form action="#" method="POST"><input style="color: lime; background: black;" type="text" placeholder="Username" name="user"><br><br><input style="color: lime; background: black;" type="password" placeholder="Password" name="pass"><br><br><input style="color: lime; background: black;" type="submit" value="Login" name="login"></center></body></html>';
 
-   $login = @($_POST['login']);
-   if(isset($login)) {
+    $login = @($_POST['login']);
+    if(isset($login)) {
 
-      $user = $_POST['user'];
-      $pass = $_POST['pass'];
-      
-      $q = "SELECT * FROM users WHERE username='$user'";
-      $res = mysqli_query($link, $q);
-      $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+        $q = "SELECT * FROM users WHERE username='$user'";
+        $res = mysqli_query($link, $q);
+        $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
       $count = mysqli_num_rows($res);
       if($count == 1 && $row['password'] == $pass) {
  
