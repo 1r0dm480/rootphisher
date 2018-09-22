b92552822fd57806657d811f1335f352f409f48e
Eduardo <41192980+blueudp@users.noreply.github.com>
2018-09-22 16:10:12 +0200

    Update index.php

--- Server/index.php
+++ Server/index.php
@@ -5,14 +5,9 @@ session_start();
 $sess = $_SESSION['username'];
 
 if(isset($sess)) {
-
-   header("Location: panel.php");
-
+    header("Location: panel.php");
 } else {
-
-   header("Location: login.php");
-
+    header("Location: login.php");
 }
-
 
 ?> 
